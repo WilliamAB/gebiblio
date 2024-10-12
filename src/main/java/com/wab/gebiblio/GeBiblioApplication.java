71ec5f2b722d9c3c8206cb7f0c589f8759e016cd
William Alberto Bertoldi <william.bertoldi@gmail.com>
2024-10-12 14:45:57 -0300

    Initial commit

---
+++ src/main/java/com/wab/gebiblio/GeBiblioApplication.java
@@ -0,0 +1,13 @@
+package com.wab.gebiblio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeBiblioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeBiblioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wab/gebiblio/GeBiblioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wab.gebiblio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeBiblioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
